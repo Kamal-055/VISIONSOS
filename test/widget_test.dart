bea955849171e1808ed658610590e123dabe0785
V KAMALA KANNAN <kamalakannan1102@gmail.com>
2026-07-04 23:31:54 +0530

    Fix import statement formatting in widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:vision/main.dart';
+import 'package:vision/main.dart'; 
 
 void main() {
   testWidgets('App compiles and loads successfully smoke test', (WidgetTester tester) async {
